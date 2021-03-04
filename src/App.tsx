f37476da7c3ab2baf5d047605a7306fe607cf863
Evan Roman <evanroman1@gmail.com>
2021-03-04 00:38:26 -0800

    adds rust wasm for computing hurst exponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import TimeSeriesChart from "./TimeSeriesChart";
 import nile from "./datasets/nile.json";
 
 const App: React.FC = () => {
+  const [rsSimple, setRsSimple] = useState(0);
+  const [rsCorrected, setRsCorrected] = useState(0);
+
+  import("hurst").then((module) => {
+    setRsSimple(module.rssimple(new Float64Array(nile.map(({ y }) => y))));
+    setRsCorrected(
+      module.rs_corrected(new Float64Array(nile.map(({ y }) => y)))
+    );
+  });
   return (
     <div className="App">
       <TimeSeriesChart
@@ -10,6 +19,8 @@ const App: React.FC = () => {
           return { x: new Date(x), y: y };
         })}
       />
+      <div>{rsSimple}</div>
+      <div>{rsCorrected}</div>
     </div>
   );
 };
