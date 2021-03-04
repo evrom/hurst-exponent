28e06ef0df5f8ce40bcf24349a300d5452ee8d3e
Evan Roman <evanroman1@gmail.com>
2021-03-04 11:23:41 -0800

    adds utilites and bitcoin price dataset

---
+++ utilities/cryptodatadownload_csv_to_json.py
@@ -0,0 +1,15 @@
+import argparse
+import pandas as pd
+
+parser = argparse.ArgumentParser()
+parser.add_argument("source")
+parser.add_argument("target")
+args = parser.parse_args()
+
+source = pd.read_csv(args.source, skiprows=1)
+x = pd.to_datetime(source['Date']).dt.strftime("%Y-%m-%d")
+y = source['Open']
+result = pd.DataFrame({'x': x, 'y': y})
+result.to_json(args.target, orient="records")
+
+
