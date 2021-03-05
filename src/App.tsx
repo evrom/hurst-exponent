16d5734dc15d753076ca39e6e6fee9760c3c7e86
Evan Roman <evanroman1@gmail.com>
2021-03-05 01:19:11 -0800

    adds description

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import "./App.css";
 import TimeSeriesChart from "./TimeSeriesChart";
 import HurstEstimations from "./HurstEstimations";
-import { datasets, metadata } from "./datasets";
+import datasets from "./datasets";
 
 const App: React.FC = () => {
   const [datasetName, setDatasetName] = useState("nile");
@@ -10,28 +10,66 @@ const App: React.FC = () => {
   return (
     <div className="App">
       <h1>Hurst Exponent</h1>
-      <p>about the hurst exponent</p>
-      {Object.entries(metadata).map(([k, v]) => {
+      <p>
+        The{" "}
+        <a href="https://en.wikipedia.org/wiki/Hurst_exponent">
+          Hurst Exponent
+        </a>{" "}
+        is a metric of the long term memory of a time series. Harold Edwin Hurst
+        developed the technique, and Benoît Mandelbrot explored it further. For
+        an introduction to the topic, see{" "}
+        <a href="http://dx.doi.org/10.3390/e19090437">
+          {`"`}A brief history of long memory: Hurst, Mandelbrot and the road to
+          ARFIMA{`"`} (2017)
+        </a>
+      </p>
+      {Object.entries(datasets).map(([k, { title }]) => {
         return (
           <button
             key={k}
             disabled={datasetName == k}
             onClick={() => setDatasetName(k)}
           >
-            {v.title}
+            {title}
           </button>
         );
       })}
       <TimeSeriesChart
-        data={datasets[datasetName].map(({ x, y }) => {
+        data={datasets[datasetName].data.map(({ x, y }) => {
           return { x: new Date(x), y: y };
         })}
       />
       <h2>Hurst Estimations</h2>
-      <p>What it means</p>
       <HurstEstimations
-        data={new Float64Array(datasets[datasetName].map(({ y }) => y))}
+        data={new Float64Array(datasets[datasetName].data.map(({ y }) => y))}
       ></HurstEstimations>
+      <p>
+        calculated using <a href="https://crates.io/crates/hurst">Hurst</a>{" "}
+        compiled to WebAssembly
+      </p>
+      <h2>Interpretation</h2>
+      <ul>
+        <li>
+          H {"="} 0.5, then the time series is Brownian. Meaning there is no
+          detectable long term memory, and each change in value appears to be
+          random and not autocorrelated
+        </li>
+        <li>
+          H {"<"} 0.5, then the time series is negatively autocorrelated. I.e.
+          there is a negative feedback loop. A high value signifies a low value
+          with come in the future, while a low value signifies a high value in
+          the future.
+        </li>
+        <li>
+          H {">"} 0.5, then the time series is positively autocorrelated. I.e. A
+          high value signifies a higher value with come in the future, while a
+          low value signifies a lower value in the future. Most real world time
+          series have a Hurst exponent above 0.5, and often around 0.72.
+          Financial data tends to be higher than natural phenomena, because
+          there are more positive feedback loops, less negative loops, and --
+          for pricing -- the inflation pushes the value upward exponentially.
+        </li>
+      </ul>
     </div>
   );
 };
--- src/datasets/index.ts
+++ src/datasets/index.ts
@@ -2,31 +2,29 @@ import nile from "./nile.json";
 import btc from "./btc.json";
 import corn from "./corn.json";
 
-type Data = {
+type DataPoint = {
   y: number;
   x: string;
 };
 
-const datasets: Record<string, Data[]> = {
-  nile: nile,
-  btc: btc,
-  corn: corn,
-};
-
-type Metadata = {
+type Dataset = {
   title: string;
+  data: DataPoint[];
 };
 
-const metadata: Record<string, Metadata> = {
+const datasets: Record<string, Dataset> = {
   nile: {
     title: "Nile water levels 1871-1984",
+    data: nile,
   },
   btc: {
     title: "BTC/USD 2015-2021",
+    data: btc,
   },
   corn: {
     title: "Corn futures opening 2015-2021",
+    data: corn,
   },
 };
 
-export { datasets, metadata };
+export default datasets;
